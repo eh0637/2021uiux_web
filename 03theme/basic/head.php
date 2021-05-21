f5c0159c0c71b567445a562ca3a014909365c44d
eh0637 <kimeunh429@gmail.com>
2021-05-21 18:20:28 +0900

    up

--- 03theme/basic/head.php
+++ 03theme/basic/head.php
@@ -29,10 +29,10 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
     <div id="hd_wrapper">
 
         <div id="logo">
-            <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_IMG_URL ?>/logo.png" alt="<?php echo $config['cf_title']; ?>"></a>
+            <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_THEME_IMG_URL ?>/logo.png" alt="<?php echo $config['cf_title']; ?>"></a>
         </div>
 
-3
+
         <ul class="hd_login">
             <?php if ($is_member) {  ?>
             <li><a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=<?php echo G5_BBS_URL ?>/register_form.php">정보수정</a></li>
@@ -167,11 +167,44 @@ $(function(){
 <?}?>
 
 
+<? if(!defined('_INDEX_')){ ?>
+<div class="subVisual">
+  <div class="subImg" id="page_title">
+
+  <div class="title">
+    <h2 class="loc1D"></h2>
+      <div class="txt">안녕하세요</div>
+  </div>
+
+  </div>
+</div>
+
+<script>
+window.onload = function(){
+  const menuDep = $(".loc1D").html(); //get
+  console.log("현재위치" + menuDep  );
+  if( menuDep == "회사소개"){
+    $(".subVisual .txt").html("저희 홈페이지를 찾아주셔서 감사합니다.");
+  }else  if( menuDep == "제품소개") {
+    $(".subVisual .txt").html("제품소개입니다.");
+  }else  if( menuDep == "커뮤니티") {
+    $(".subVisual .txt").html("자유롭게 커뮤니티하실수 있는 곳입니다.");
+  }
+
+};
+
+
+</script>
+
+  <?}?>
+
 
 <!-- 콘텐츠 시작 { -->
 <div id="wrapper">
     <div id="container_wr">
       <!-- 1200px -->
 
-    <div id="container" <?if(defined('_INDEX_')){?>style="width:1200px;"<?}?>   <!-- 930 -->
-        <?php if (!defined("_INDEX_")) { ?><h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
+    <div id="container" <?if(defined('_INDEX_')){?>style="width:1200px;"<?}?> >  <!-- 930 -->
+        <?php if (!defined("_INDEX_")) { ?>
+          <div>H> <span class="loc1D"> </span> > <?php echo get_text($g5['title']); ?> </div>
+          <h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
